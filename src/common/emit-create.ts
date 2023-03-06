56bf3827c9ac60cb331f7ee06a119d64a128668c
Emmanuel Bernard <emmanuel.bernard@liip.ch>
2023-03-06 03:30:33 +0100

    Fix map preview

--- src/common/emit-create.ts
+++ src/common/emit-create.ts
@@ -8,6 +8,7 @@ export const emitCreate = async (happening, mode, game) => {
   data.name = happening.name;
   data.description = happening.description;
   data.nft_ticket_address = happening.address;
+  data.url_map_preview = mode.banner;
 
   // Fix for ALR
   data.title = data.name;
